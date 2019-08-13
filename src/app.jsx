01b00cfba40ca4ed590f1d656c088bd1f0a2da6e
cail <cail1844@gmail.com>
2019-08-14 01:09:49 +0800

    fet: 增加stylelintrc配置文件 fet: 使用taoro-ui组件和taro-ui自定义样式和主题

--- src/app.jsx
+++ src/app.jsx
@@ -6,7 +6,8 @@ import Index from './pages/index'
 
 import configStore from './store'
 
-import './app.scss'
+// import './app.scss'
+import './assets/styles/app.scss'
 
 // 如果需要在 h5 环境中开启 React Devtools
 // 取消以下注释：
