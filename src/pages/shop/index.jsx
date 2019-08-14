8ac285579c95b3111506c42425bc25ed9de7dde4
cail <cail1844@gmail.com>
2019-08-14 21:47:04 +0800

    fet: 促销产品徽章

--- src/pages/shop/index.jsx
+++ src/pages/shop/index.jsx
@@ -1,5 +1,6 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Image, Text } from '@tarojs/components'
+import { AtBadge } from "taro-ui";
 import SearchBar from "../../components/search-bar"
 
 
@@ -36,7 +37,15 @@ class ShopIndex extends Component {
               <View key={product.id} className='card mb-2'>
                 <Image className='card-img-top' src={product.images[0].src} mode='aspectFit' />
                 <View className='card-body text-center'>
-                  <View className='card-title mb-2'>{product.name}</View>
+                  {/*<View className='card-title mb-2'>{product.name}</View>*/}
+                  <View className='card-title mb-2'>
+                    <View className='card-title-text'>
+                      {product.on_sale &&
+                        <AtBadge className='card-title-badge' value='sale' />
+                      }
+                      {product.name}
+                    </View>
+                  </View>
                   <View className='card-subtitle'>
                     {product.on_sale && <Text className='mr-2 text-muted text-through'>{'￥' + product.regular_price}</Text>}
                     <Text>{'￥' + product.price}</Text>
