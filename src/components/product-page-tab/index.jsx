6aef817fd79900e97bdb01ed657bcd84f7938c70
cail <cail1844@gmail.com>
2019-08-17 21:40:25 +0800

    fet: 产品页面标签组件

---
+++ src/components/product-page-tab/index.jsx
@@ -0,0 +1,49 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View } from '@tarojs/components'
+import { AtList, AtListItem, AtTabs, AtTabsPane } from "taro-ui";
+import RichTextWxParse from "../rich-text-wx-parse";
+
+class ProductPageTab extends Component {
+  static options = {
+    addGlobalClass: true
+  }
+
+  static defaultProps = {
+    data: {"attributes": []},
+    tabList: [],
+    activeTab: 0,
+    onClick: () => {}
+  }
+
+  render() {
+    const {data: product, tabList, activeTab} = this.props
+    return (
+      <View className='mx-3 my-5'>
+        <AtTabs
+          current={activeTab}
+          tabList={tabList}
+          onClick={this.props.onClick.bind(this)}
+        >
+          <AtTabsPane className='mt-4' current={activeTab} index={0}>
+            <RichTextWxParse content={product.description} />
+          </AtTabsPane>
+          <AtTabsPane className='mt-4' current={activeTab} index={1}>
+            <AtList>
+              {product.attributes.map(attr =>
+                <AtListItem
+                  key={attr.id}
+                  hasBorder={false}
+                  title={attr.name}
+                  note={attr.options.toString()}
+                />
+              )
+              }
+            </AtList>
+          </AtTabsPane>
+        </AtTabs>
+      </View>
+    )
+  }
+}
+
+export default ProductPageTab
