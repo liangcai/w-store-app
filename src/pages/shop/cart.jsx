c8117897686104090188cdaff5d419db56e7751c
cail <cail1844@gmail.com>
2019-08-19 13:58:59 +0800

    fet: 获取购物车项目

--- src/pages/shop/cart.jsx
+++ src/pages/shop/cart.jsx
@@ -1,11 +1,44 @@
 import Taro, {Component} from '@tarojs/taro'
 import {View} from '@tarojs/components'
+import fetchData from "../../utilities/fetch-data"
 
 class ShopCart extends Component {
   config = {
     navigationBarTitleText: "购物车"
   }
 
+  state = {
+    cart: {
+      total: 0,
+      items: []
+    }
+  }
+
+  constructor() {
+    super(...arguments)
+    this.fetchData = fetchData
+  }
+
+  getCartSuccess(response) {
+    this.setState({
+      cart: response.data
+    })
+  }
+
+  getCart() {
+    this.fetchData({
+      resource: 'shopping-cart',
+      success: this.getCartSuccess.bind(this)
+    })
+  }
+
+  componentWillMount() {
+    this.getCart()
+  }
+
+  componentDidShow() {
+    this.getCart()
+  }
 
   render() {
     return (
