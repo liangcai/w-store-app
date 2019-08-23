522f29a8c7bd15abd64b6fb223137fb97a8b6c30
cail <cail1844@gmail.com>
2019-08-23 22:53:05 +0800

    feat: 请求绑定微信账户的接口和绑定微信账户功能     后端接口处理所有绑定可能遇到的异常，前端显示接口返回的提示信息     后端正确绑定后自动登录用户，返回结果让前端刷新

--- service/modules/user/index.js
+++ service/modules/user/index.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken')
 const fs = require('fs')
 const path = require('path')
 const http = require('axios')
+const _ = require('lodash')
 const {db} = require('../../db')
 const {authMiddleware} = require('./middleware')
 
@@ -119,10 +120,12 @@ router.post('/token/validate', authMiddleware, (req, res) => {
 
 // 微信登录
 router.post('/wx-login', async (req, res) => {
+  db.read()
   const {code} = req.body
 
   if (!code) {
     res.status(400).jsonp('微信登录失败，请重试！')
+    return
   }
 
   try {
@@ -131,12 +134,63 @@ router.post('/wx-login', async (req, res) => {
 
     if (!user) {
       res.status(404).jsonp('还没有绑定微信账户。')
+      return
     }
 
     res.jsonp('ok')
   } catch (error) {
     res.status(500).jsonp(error.message)
   }
+})
+
+//微信账户绑定
+router.post('/wx-bind', async (req, res) => {
+  db.read()
+  const {username, password, wxUserInfo, code} = req.body
+
+  //查找绑定用户名是否存在
+  const user = getUserByName(username)
+  if (!user) {
+    res.status(404).jsonp('用户名不存在。')
+    return
+  }
+  //验证密码
+  const passwordMatch = await bcrypt.compare(password, user.password)
+  if (!passwordMatch) {
+    res.status(403).jsonp('密码不正确。')
+    return
+  }
+  // 确定用户是否已经绑定了某个微信
+  if (_.has(user, 'wexin.openid')) {
+    res.status(403).jsonp('这个用户已经绑定了微信账户。')
+    return
+  }
+  //如果用户还没有绑定微信，就去获取用户微信会话
+  const sessionData = await getWxSession(code)
+  // 确定openid是否已经绑定了其他账户，感觉这一步不一定需要，如果绑定了，岂不是直接就微信登录了？
+  const openidIsUsed = getUserByOpenid(sessionData.openid)
+  if(openidIsUsed) {
+    res.status(400).jsonp('微信账户已经与其他用户绑定了')
+    return
+  }
+  // 存储微信数据
+  const weixin = {
+    openid: sessionData.openid,
+    userInfo: wxUserInfo.userInfo
+  }
+
+  db.get('users')
+    .find({username: user.username})
+    .assign({weixin})
+    .write()
+
+  // 让用户登录
+  const token = signToken(user)
+  res.jsonp({
+    id: user.id,
+    username: user.username,
+    token
+  })
 })
 
 module.exports = router
